e95f7d047b36382280f7135ec021c9c057b505d1
Carlos Chang <chiencarlos11@gmail.com>
2021-03-10 22:43:08 -0500

    adding id field back to course serializer

--- core/serializers.py
+++ core/serializers.py
@@ -34,6 +34,6 @@ class CourseSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Course
-        fields = ['Name', 'Location', 'PriceModel', 'Date','Provider','link','logo','isArchived']
+        fields = ['id','Name', 'Location', 'PriceModel', 'Date','Provider','link','logo','isArchived']
 
 
