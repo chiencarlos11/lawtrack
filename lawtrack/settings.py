d96d3351d9a57e1021aea9697d74a9e831f3fa28
Carlos Chang <chiencarlos11@gmail.com>
2021-03-17 22:19:27 -0400

    postgresql

--- lawtrack/settings.py
+++ lawtrack/settings.py
@@ -131,8 +131,8 @@ REST_FRAMEWORK = {
     ]
 }
 
-if 'DATABASE_URL' in os.environ:
-    import dj_database_url
-    DATABASES = {'default': dj_database_url.config()}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
 
 django_heroku.settings(locals()) 
